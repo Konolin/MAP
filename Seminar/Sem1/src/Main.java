4fca543c32bcb90141d1b2ede1e93023923ec596
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-05 10:28:55 +0300

    Ex 1 si 2

--- Seminar/Sem1/src/Main.java
+++ Seminar/Sem1/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Operations op = new Operations();
+
+        System.out.println(op.add(10, 10));
+        System.out.println(op.sub(10, 10));
+
+        System.out.println(reverse("string"));
+    }
+
+    public static String reverse(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            char ch = input.charAt(i);
+            sb.append(ch);
+        }
+
+        return sb.toString();
     }
 }
