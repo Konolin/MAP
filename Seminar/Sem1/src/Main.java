611da34aa96f1bf23130fac84a5b7babfd13e042
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-05 10:59:37 +0300

    Ex 3

--- Seminar/Sem1/src/Main.java
+++ Seminar/Sem1/src/Main.java
@@ -2,13 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Operations op = new Operations();
 
-        System.out.println(op.add(10, 10));
-        System.out.println(op.sub(10, 10));
+//        System.out.println(op.add(10, 10));
+//        System.out.println(op.sub(10, 10));
+//
+//        System.out.println(reverse("string"));
+//
+//        Student st = new Student("Idk", 19);
+//        System.out.println(st.toString());
 
-        System.out.println(reverse("string"));
-
-        Student st = new Student("Idk", 19);
-        System.out.println(st.toString());
+        System.out.println(wordCount(" nu  stiu ce   ec  "));
     }
 
     public static String reverse(String input) {
@@ -20,5 +22,30 @@ public class Main {
         }
 
         return sb.toString();
+    }
+
+    public static int wordCount(String sentence) {
+        int i = 0;
+        while (i < sentence.length() && !Character.isLetter(sentence.charAt(i))) {
+            i++;
+        }
+
+        int result = 0;
+        while (i < sentence.length()) {
+            while (i < sentence.length() && sentence.charAt(i) == ' ') {
+                i++;
+            }
+
+            if (i == sentence.length()) {
+                break;
+            }
+            result++;
+
+            while (i < sentence.length() && sentence.charAt(i) != ' ') {
+                i++;
+            }
+        }
+
+        return result;
     }
 }
