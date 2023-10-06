4bde7fed38e7cfcaf01be2fbb899db803568f913
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-06 12:51:02 +0300

    Main

--- Laborator/Lab1/src/Ex1/Main/GPMain.java
+++ Laborator/Lab1/src/Ex1/Main/GPMain.java
@@ -1,7 +1,13 @@
 package Ex1.Main;
 
+import java.util.Arrays;
+
 public class GPMain {
     public static void main(String[] args) {
-        GradeProcessor noteProcessor = new GradeProcessor();
+        GradeProcessor gradeProcessor = new GradeProcessor();
+
+        int[] grades = new int[] { 10, 20, 30, 39, 47, 48, 60 };
+
+        System.out.println(Arrays.toString(gradeProcessor.insufficientGrades(grades)));
     }
 }
