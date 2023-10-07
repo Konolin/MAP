4a7ebfa8941a076b5e91ec384b6c85f48cf4169f
Konolin <mandru.catalin03@gmail.com>
2023-10-07 11:59:47 +0300

    Array Processor complete

---
+++ Laborator/Lab1/src/Ex2/Main/ArrayProcessor.java
@@ -0,0 +1,43 @@
+package Ex2.Main;
+
+public class ArrayProcessor {
+    public int maxElem(int[] array) {
+        int max = Integer.MIN_VALUE;
+        for (int elem : array) {
+            if (elem > max) {
+                max = elem;
+            }
+        }
+        return max;
+    }
+
+    public int minElem(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int elem : array) {
+            if (elem < min) {
+                min = elem;
+            }
+        }
+        return min;
+    }
+
+    public int maxSum(int[] array) {
+        int sum = sumArray(array);
+        int min = minElem(array);
+        return sum - min;
+    }
+
+    public int minSum(int[] array) {
+        int sum = sumArray(array);
+        int max = minElem(array);
+        return sum - max;
+    }
+
+    public int sumArray(int[] array) {
+        int sum = 0;
+        for (int elem : array) {
+            sum += elem;
+        }
+        return sum;
+    }
+}
--- Laborator/Lab1/src/Ex2/Main/Ex2Main.java
+++ Laborator/Lab1/src/Ex2/Main/Ex2Main.java
@@ -2,46 +2,9 @@ package Ex2.Main;
 
 public class Ex2Main {
     public static void main(String[] args) {
+        ArrayProcessor ap = new ArrayProcessor();
 
-    }
-
-    public int maxElem(int[] array) {
-        int max = Integer.MIN_VALUE;
-        for (int elem : array) {
-            if (elem > max) {
-                max = elem;
-            }
-        }
-        return max;
-    }
-
-    public int minElem(int[] array) {
-        int min = Integer.MAX_VALUE;
-        for (int elem : array) {
-            if (elem < min) {
-                min = elem;
-            }
-        }
-        return min;
-    }
-
-    public int maxSum(int[] array) {
-        int sum = sumArray(array);
-        int min = minElem(array);
-        return sum - min;
-    }
-
-    public int minSum(int[] array) {
-        int sum = sumArray(array);
-        int max = minElem(array);
-        return sum - max;
-    }
-
-    public int sumArray(int[] array) {
-        int sum = 0;
-        for (int elem : array) {
-            sum += elem;
-        }
-        return sum;
+        int[] example = new int[]{4, 8, 3, 10, 17};
+        System.out.println(ap.minSum(example));
     }
 }
