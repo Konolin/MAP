4445138a032a2a489bd02468a4bb81efeffa7942
Konolin <mandru.catalin03@gmail.com>
2023-10-07 14:04:12 +0300

    Complete electronics shop

--- Laborator/Lab1/src/Ex4/Main/ElectronicsShop.java
+++ Laborator/Lab1/src/Ex4/Main/ElectronicsShop.java
@@ -1,4 +1,51 @@
 package Ex4.Main;
 
+import java.net.Inet4Address;
+
 public class ElectronicsShop {
+    public int cheapestItem(int[] itemArray) {
+        int min = Integer.MAX_VALUE;
+        for (int item : itemArray) {
+            if (min > item) {
+                min = item;
+            }
+        }
+        return min;
+    }
+
+    public int mostExpensiveItem(int[] itemArray) {
+        int max = Integer.MIN_VALUE;
+        for (int item : itemArray) {
+            if (max < item) {
+                max = item;
+            }
+        }
+        return max;
+    }
+
+    public int mostExpensiveAndAffordableItem(int[] itemArray, int budget) {
+        int max = Integer.MIN_VALUE;
+        for (int item : itemArray) {
+            if (max < item && item < budget) {
+                max = item;
+            }
+        }
+        return max;
+    }
+
+    public int bestCombinationPrice(int[] itemArray1, int[] itemArray2, int budget) {
+        int maxSum = 0;
+        for (int i = 0; i < itemArray1.length; i++) {
+            for (int j = 0; j < itemArray2.length; j++) {
+                int currentSum = itemArray1[i] + itemArray2[j];
+                if (currentSum <= budget && currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+            }
+        }
+        if (maxSum == 0) {
+            return -1;
+        }
+        return maxSum;
+    }
 }
--- Laborator/Lab1/src/Ex4/Main/Ex4Main.java
+++ Laborator/Lab1/src/Ex4/Main/Ex4Main.java
@@ -1,4 +1,18 @@
 package Ex4.Main;
 
 public class Ex4Main {
+    public static void main(String[] args) {
+        int[] keyboards = new int[]{40, 50, 60};
+        int[] usb = new int[]{8, 12};
+
+        ElectronicsShop es = new ElectronicsShop();
+        System.out.println(es.cheapestItem(keyboards));
+
+        System.out.println(es.mostExpensiveItem(keyboards));
+        System.out.println(es.mostExpensiveItem(usb));
+
+        System.out.println(es.mostExpensiveAndAffordableItem(usb, 10));
+
+        System.out.println(es.bestCombinationPrice(keyboards, usb, 60));
+    }
 }
