a198b4b86b73e08abb654c9dbe44bfd2a2d688e5
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-13 15:18:12 +0300

    Complete ex4

--- Laborator/Lab1/src/Ex4/Main/ElectronicsShop.java
+++ Laborator/Lab1/src/Ex4/Main/ElectronicsShop.java
@@ -1,9 +1,9 @@
 package Ex4.Main;
 
-import java.net.Inet4Address;
-
 public class ElectronicsShop {
     public int cheapestItem(int[] itemArray) {
+        validateArray(itemArray);
+
         int min = Integer.MAX_VALUE;
         for (int item : itemArray) {
             if (min > item) {
@@ -14,6 +14,8 @@ public class ElectronicsShop {
     }
 
     public int mostExpensiveItem(int[] itemArray) {
+        validateArray(itemArray);
+
         int max = Integer.MIN_VALUE;
         for (int item : itemArray) {
             if (max < item) {
@@ -24,6 +26,9 @@ public class ElectronicsShop {
     }
 
     public int mostExpensiveAndAffordableItem(int[] itemArray, int budget) {
+        validateArray(itemArray);
+        validateBudget(budget);
+
         int max = Integer.MIN_VALUE;
         for (int item : itemArray) {
             if (max < item && item < budget) {
@@ -34,6 +39,10 @@ public class ElectronicsShop {
     }
 
     public int bestCombinationPrice(int[] itemArray1, int[] itemArray2, int budget) {
+        validateArray(itemArray1);
+        validateArray(itemArray2);
+        validateBudget(budget);
+
         int maxSum = 0;
         for (int i = 0; i < itemArray1.length; i++) {
             for (int j = 0; j < itemArray2.length; j++) {
@@ -47,5 +56,22 @@ public class ElectronicsShop {
             return -1;
         }
         return maxSum;
+    }
+
+    private void validateArray(int[] array) {
+        if (array.length == 0) {
+            throw new RuntimeException("Array should not be empty.");
+        }
+        for (int price : array) {
+            if (price < 0) {
+                throw new RuntimeException("Prices can't be negative.");
+            }
+        }
+    }
+
+    private void validateBudget(int budget) {
+        if (budget < 0) {
+            throw new RuntimeException("Budget should not be negative.");
+        }
     }
 }
--- Laborator/Lab1/src/Ex4/Test/Ex4Test.java
+++ Laborator/Lab1/src/Ex4/Test/Ex4Test.java
@@ -1,4 +1,58 @@
 package Ex4.Test;
 
+import Ex4.Main.ElectronicsShop;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class Ex4Test {
+    private ElectronicsShop es;
+    private final static String SHOULD_BE_EQUAL = "Should be equal";
+    private final static String SHOULD_THROW_EXCEPTION = "Should throw an exception";
+    private int[] items;
+    private int[] wrong;
+
+    @Before
+    public void setup() {
+        es = new ElectronicsShop();
+        items = new int[]{13, 531, 42, 94, 31};
+        wrong = new int[]{13, 531, -42, 94, 31};
+    }
+
+    @Test
+    public void cheapestItemTest() {
+        int result = es.cheapestItem(items);
+        assertEquals(13, result, SHOULD_BE_EQUAL);
+        assertThrows(RuntimeException.class, () -> es.cheapestItem(wrong), SHOULD_THROW_EXCEPTION);
+        assertThrows(RuntimeException.class, () -> es.cheapestItem(new int[0]), SHOULD_THROW_EXCEPTION);
+    }
+
+    @Test
+    public void mostExpensiveItemTest() {
+        int result = es.mostExpensiveItem(items);
+        assertEquals(531, result, SHOULD_BE_EQUAL);
+        assertThrows(RuntimeException.class, () -> es.mostExpensiveItem(wrong), SHOULD_THROW_EXCEPTION);
+        assertThrows(RuntimeException.class, () -> es.mostExpensiveItem(new int[0]), SHOULD_THROW_EXCEPTION);
+    }
+
+    @Test
+    public void mostExpensiveAndAffordableItemTest() {
+        int result = es.mostExpensiveAndAffordableItem(items, 60);
+        assertEquals(42, result, SHOULD_BE_EQUAL);
+        assertThrows(RuntimeException.class, () -> es.mostExpensiveAndAffordableItem(wrong, 60), SHOULD_THROW_EXCEPTION);
+        assertThrows(RuntimeException.class, () -> es.mostExpensiveAndAffordableItem(items, -40), SHOULD_THROW_EXCEPTION);
+    }
+
+    @Test
+    public void bestCombinationPriceTest() {
+        int[] items2 = new int[] {42, 246, 57, 31, 423};
+        int result = es.bestCombinationPrice(items, items2, 155);
+        assertEquals(151, result, SHOULD_BE_EQUAL);
+        result = es.bestCombinationPrice(items, items2, 10);
+        assertEquals(-1, result, SHOULD_BE_EQUAL);
+        assertThrows(RuntimeException.class, ()-> es.bestCombinationPrice(wrong, items, 90), SHOULD_THROW_EXCEPTION);
+        assertThrows(RuntimeException.class, ()-> es.bestCombinationPrice(items, items2, -90), SHOULD_THROW_EXCEPTION);
+    }
 }
