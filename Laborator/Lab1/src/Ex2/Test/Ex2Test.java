b21c231f3cd598b38e8b0321816006ef6cc195f4
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-13 12:57:26 +0300

    Complete Ex2

--- Laborator/Lab1/src/Ex2/Test/Ex2Test.java
+++ Laborator/Lab1/src/Ex2/Test/Ex2Test.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class Ex2Test {
     private ArrayProcessor ap;
     private static final String SHOULD_BE_EQUAL = "Should be equal";
+    private static final String SHOULD_THROW_EXCEPTION = "Should throw exception";
 
     @BeforeEach
     public void setUp() {
@@ -21,7 +22,7 @@ public class Ex2Test {
         assertEquals(53, ap.maxElem(new int[]{0, 5, 1, 53, 23, 15, -14}), SHOULD_BE_EQUAL);
         assertEquals(0, ap.maxElem(new int[]{0, 0, 0, 0}), SHOULD_BE_EQUAL);
         assertEquals(-10, ap.maxElem(new int[]{-40, -10, -31, -120}), SHOULD_BE_EQUAL);
-        assertThrows(RuntimeException.class, () -> ap.maxElem(new int[0]));
+        assertThrows(RuntimeException.class, () -> ap.maxElem(new int[0]), SHOULD_THROW_EXCEPTION);
     }
 
     @Test
@@ -29,21 +30,24 @@ public class Ex2Test {
         assertEquals(-14, ap.minElem(new int[]{0, 5, 1, 53, 23, 15, -14}), SHOULD_BE_EQUAL);
         assertEquals(0, ap.minElem(new int[]{0, 0, 0, 0}), SHOULD_BE_EQUAL);
         assertEquals(-120, ap.minElem(new int[]{-40, -10, -31, -120}), SHOULD_BE_EQUAL);
-        assertThrows(RuntimeException.class, () -> ap.minElem(new int[0]));
+        assertThrows(RuntimeException.class, () -> ap.minElem(new int[0]), SHOULD_THROW_EXCEPTION);
     }
 
     @Test
-    public void maxSum() {
+    public void maxSumTest() {
         assertEquals(39, ap.maxSum(new int[]{4, 8, 3, 10, 17}), SHOULD_BE_EQUAL);
+        assertThrows(RuntimeException.class, () -> ap.maxSum(new int[0]), SHOULD_THROW_EXCEPTION);
     }
 
     @Test
-    public void minSum() {
+    public void minSumTest() {
         assertEquals(25, ap.minSum(new int[]{4, 8, 3, 10, 17}), SHOULD_BE_EQUAL);
+        assertThrows(RuntimeException.class, () -> ap.minSum(new int[0]), SHOULD_THROW_EXCEPTION);
     }
 
     @Test
-    public void arraySum() {
+    public void arraySumTest() {
         assertEquals(42, ap.arraySum(new int[]{4, 8, 3, 10, 17}), SHOULD_BE_EQUAL);
+        assertThrows(RuntimeException.class, () -> ap.arraySum(new int[0]), SHOULD_THROW_EXCEPTION);
     }
 }
